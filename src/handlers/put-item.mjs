0f59e0e258cf742863ac7032f316000e406b9d34
Jose Corredor <jose96corrza@gmail.com>
2024-06-19 18:16:07 -0500

    Removed unused lambda, also fix the iot rule. and adjust the lambda to parse the object coming from iot core topic

--- src/handlers/put-item.mjs
+++ src/handlers/put-item.mjs
@@ -3,6 +3,7 @@
 // Create a DocumentClient that represents the query to add an item
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { v4 as uuidV4 } from 'uuid'
 const client = new DynamoDBClient({});
 
 const ddbDocClient = DynamoDBDocumentClient.from(client);
@@ -19,14 +20,37 @@ export const putItemHandler = async (event) => {
     console.info('received:', event);
 
     // Get id and name from the body of the request
-    // const body = JSON.parse(event.body);
+    const id = uuidV4()
+    const eventString = JSON.stringify(event)
+    const body = JSON.parse(eventString);
 
+  //   {
+  //     "device": "rpi-master",
+  //     "mode": "STATION",
+  //     "signal": null,
+  //     "data": {
+  //         "pressure": {
+  //             "value": "1023",
+  //             "magnitude": "Pa"
+  //         },
+  //         "humidity": {
+  //             "value": "80",
+  //             "magnitude": "%"
+  //         }
+  //     }
+  // }
 
     // Creates a new item, or replaces an old item with a new item
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
     var params = {
         TableName : tableName,
-        Item: { id : "1", name: 'test', test: 'IoTRule' }
+        Item: { 
+          id : id,
+          pressure_val: body.data.pressure.value, 
+          pressure_mag: body.data.pressure.magnitude, 
+          humidity_val: body.data.humidity.value,
+          humidity_mag: body.data.humidity.magnitude 
+         }
     };
 
     // var paramsIot = {
@@ -39,9 +63,7 @@ export const putItemHandler = async (event) => {
         const data = await ddbDocClient.send(new PutCommand(params));
 
 
-        console.log(result);
         console.log("Success - item added or updated", data);
-        console.log("DATA", event);
       } catch (err) {
         console.log("Error", err.stack);
       }
